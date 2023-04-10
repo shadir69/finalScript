b43d09bc6bcbbdfa321197138f33e633a169cf68
Hachadi Younes <38957167+shadir69@users.noreply.github.com>
2023-04-10 06:04:25 +0100

    Update noest.user.js

--- noest.user.js
+++ noest.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        NoEst Final UserScript
+// @name        NoEst Final UserScript V1.1
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
